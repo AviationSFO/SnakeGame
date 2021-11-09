af7dc556648ff53de4a055ba3d923babd540cafc
AviationSFO <srw08sf@gmail.com>
2021-11-09 12:31:22 -0800

    1.2.4

---
+++ launcher.py
@@ -0,0 +1,18 @@
+# Launcher for Snake Game by Steven Weintein 11-9-2021
+# import required modules
+import turtle
+import os
+import time
+
+#screen setup
+wn = turtle.Screen()
+wn.setup(width = 600, height = 600)
+wn.bgcolor("white")
+snakegame = open(os.path.expanduser("~/Desktop/SnakeGame/turtlesnake.py"), "r")
+snakegame.seek(0)
+code = snakegame.read
+def launch(code):
+    exec(code)
+wn.onscreenclick(launch, 1, add=True)
+while True:
+    time.sleep(5)

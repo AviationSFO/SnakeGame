8b782f3f0d47369a20e921ce438c042f1408b21d
AviationSFO <srw08sf@gmail.com>
2022-02-08 20:03:11 -0800

    test new install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ if python3 --version | grep -q '3.6'; then
     echo "Python version checks"
 elif python3 --version | grep -q '3.7'; then
     echo "Python version checks"
-if python3 --version | grep -q '3.8'; then
+elif python3 --version | grep -q '3.8'; then
     echo "Python version checks"
-if python3 --version | grep -q '3.9'; then
+elif python3 --version | grep -q '3.9'; then
     echo "Python version checks"
-if python3 --version | grep -q '3.10'; then
+elif python3 --version | grep -q '3.10'; then
     echo "Python version checks"
 else
     echo "Your python version is too old, Snake Game may not work. Consider upgrading python to 3.6 or later."

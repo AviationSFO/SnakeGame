b086fb90baf0d6ac635bd948903371c975207e16
AviationSFO <srw08sf@gmail.com>
2022-02-09 13:55:45 -0800

    update playsound requirments

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ cd ~/Desktop
 git clone https://github.com/AviationSFO/SnakeGame
 cd SnakeGame
 python3 -m pip install playsound
+python3 -m pip install PyObjC
 echo "Your python version is:"
 python3 --version
 echo "WARNING!"

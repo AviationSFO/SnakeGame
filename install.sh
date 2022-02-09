f6f6609cadd9ddc9f47bef278228445404fbb03f
AviationSFO <srw08sf@gmail.com>
2022-02-08 20:05:45 -0800

    fix playsound version in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~/Desktop
 git clone https://github.com/AviationSFO/SnakeGame
 cd SnakeGame
-python3 -m pip install playsound2
+python3 -m pip install playsound
 echo "Your python version is:"
 python3 --version

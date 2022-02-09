a1147e87af250bbe10b9be708b868a73c291cb94
AviationSFO <srw08sf@gmail.com>
2022-02-09 13:52:28 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ cd SnakeGame
 python3 -m pip install playsound
 echo "Your python version is:"
 python3 --version
+echo "WARNING!"
+echo "If the version above is NOT 3.6 or later, Snake Game will not work, please consider upgrading to a compatible version."

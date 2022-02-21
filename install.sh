e1ab6b6a4c6f2c31f227c3269b82c9fd0c9004d6
AviationSFO <srw08sf@gmail.com>
2022-02-21 12:00:49 -0800

    update install.sh for pyenv compatibility

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 cd ~/Desktop
 git clone https://github.com/AviationSFO/SnakeGame
 cd SnakeGame
-python3 -m pip install playsound
-python3 -m pip install PyObjC
+if [ python -V -eq 2.7.16 ]
+then
+    python3 -m pip install playsound
+    python3 -m pip install PyObjC
+else
+    python -m pip install playsound
+    python -m pip install PyObjC
+fi
 echo "Your python version is:"
-python3 --version
+if [ python -V -eq 2.7.16 ]
+then
+    python3 -V
+else
+    python -V
+fi
 echo "WARNING!"
 echo "If the version above is NOT 3.6 or later, Snake Game will not work, please consider upgrading to a compatible version."

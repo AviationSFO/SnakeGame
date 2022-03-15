b3031baf79e36f94ac8bcf74f0d85c16d4d5fe47
AviationSFO <srw08sf@gmail.com>
2022-03-15 09:53:51 -0700

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
+# Is it possible to have install script clone from latest version rather than the latest commit?
+
 cd ~/Desktop
-git clone https://github.com/AviationSFO/SnakeGame
+git clone https://github.com/AviationSFO/SnakeGame/releases/latest
 cd SnakeGame
-if [ python -V = Python 2.7.16 ]
+if [ python -V = "Python 2.7.16" ]
 then
     python3 -m pip install playsound
     python3 -m pip install PyObjC
+    echo "Your python version is:"
+    python3 -V
 else
     python -m pip install playsound
     python -m pip install PyObjC
-fi
-echo "Your python version is:"
-if [ python -V = Python 2.7.16 ]
-then
-    python3 -V
-else
+    echo "Your python version is:"
     python -V
 fi
 echo "WARNING!"
